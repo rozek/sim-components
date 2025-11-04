024d65175c98c64dfaed3ce7ae3f63b986925d9b
Andreas Rozek <A.Rozek@gmx.de>
2025-11-04 17:43:20 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,19 @@ import { defineConfig } from 'vite'
 import pkg from './package.json'
 
 export default defineConfig({
+  resolve: {
+    alias: [
+      { find: 'preact/hooks', replacement: 'node_modules/preact/hooks/dist/hooks.es.js' },
+      { find: 'preact/compat', replacement: 'node_modules/preact/compat/dist/compat.es.js' }
+    ]
+  },
   build: {
     lib: {
       entry: './src/sim-components.ts',
       fileName: () => `sim-components.esm.js`,
       formats: ['es']
     },
+    minify: false,
     target: 'es2020',
     outDir: './dist',
     emptyOutDir: true,
